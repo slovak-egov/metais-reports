deccaa078815d460b6311a69458f735f9fa5eebb
Brano <rabatin.b@gmail.com>
2025-11-20 18:10:53 +0100

    Paging templates-only

--- groovy/templates/extract_relation_template.groovy
+++ groovy/templates/extract_relation_template.groovy
@@ -1,38 +1,32 @@
 def headers = [
-        new Header("central_uuid", Header.Type.STRING),
-        new Header("outer_uuid", Header.Type.STRING)
+        new Header("source_uuid", Header.Type.STRING),
+        new Header("target_uuid", Header.Type.STRING)
     ]
  
- def qi_central = qi("central")
+ def qi_target = qi("central")
  def qi_rel = qi("rel")
- def qi_outer = qi("outer")
+ def qi_source = qi("outer")
  
- def type_CENTRAL = type("__CENTRAL__")
- def type_OUTER = type("__OUTER__")
+ def type_TARGET = type("__CENTRAL__")
+ def type_SOURCE = type("__OUTER__")
  def type_REL = type("__RELATION__")
  
- def base = match(path().node(qi_central, type_CENTRAL))
- .where(not(qi_central.filter(state(StateEnum.INVALIDATED))))
+ def base = match(path().node(qi_target, type_TARGET))
  
  def q = base.match(
     path()
-    .node(qi_central)
+    .node(qi_target)
     .rel(qi_rel, RelationshipDirection.IN, type_REL)
-    .node(qi_outer, type_OUTER)
+    .node(qi_source, type_SOURCE)
     )
-    .where(and(
-    not(qi_rel.filter(state(StateEnum.INVALIDATED))),
-    not(qi_outer.filter(state(StateEnum.INVALIDATED)))
-    ))
  
- def resCount = Neo4j.execute(q.returns(count("totalCount", qi_central)))
+ def resCount = Neo4j.execute(q.returns(count("totalCount", qi_target)))
  def total = (resCount.data && resCount.data[0]?.totalCount) ? (resCount.data[0].totalCount as int) : 0
  
  def query = q.returns(
-        prop("central_uuid", qi_central.prop("\$cmdb_id")),
-        prop("outer_uuid", qi_outer.prop("\$cmdb_id"))
+        prop("source_uuid", qi_source.prop("\$cmdb_id")),
+        prop("target_uuid", qi_target.prop("\$cmdb_id"))
     )
-    .orderBy(qi_central.prop("\$cmdb_id"), OrderDirection.ASC)
  
  def res = Neo4j.execute(query)
  
---
+++ groovy/templates/relation_template.groovy
@@ -0,0 +1,27 @@
+def qi_source = qi("source")
+def qi_target = qi("target")
+def qi_rel    = qi("rel")
+
+def type_SOURCE = type("__SOURCE__")
+def type_TARGET = type("__TARGET__")
+def type_REL    = type("__RELATION__")
+
+def base = match(path().node(qi_target, type_TARGET))
+
+def q = base.match(
+    path()
+    .node(qi_target)
+    .rel(qi_rel, RelationshipDirection.IN, type_REL)
+    .node(qi_source, type_SOURCE)
+)
+
+def query = q.returns(
+    prop("source", qi_source.prop("\$cmdb_id")),
+    prop("target", qi_target.prop("\$cmdb_id"))
+).limit(__LIMIT__).offset(__OFFSET__)
+
+def res = Neo4j.execute(query)
+
+return res.data.collect { row ->
+    [ source: row.source, target: row.target ]
+}
