5bd6c335afbe1f2b52b3caf2292ed113a1a65ac5
Brano <rabatin.b@gmail.com>
2025-11-21 11:15:56 +0100

    Final templates

--- groovy/templates/extract_raw_paged_template.groovy
+++
@@ -1,9 +0,0 @@
-def qi_node   = qi("node")
-def type_node = type("__TYPE__")
-
-def q = match(path().node(qi_node, type_node))
-  .where(not(qi_node.filter(state(StateEnum.INVALIDATED))))
-
-def q2 = q.returns(node(qi_node)).limit(__LIMIT__).offset(__OFFSET__)
-def res = Neo4j.execute(q2)
-return res.data.collect { it.node }
--- groovy/templates/extract_raw_template.groovy
+++
@@ -1,8 +0,0 @@
-def qi_node = qi("node")
-def type_node = type("__TYPE__")
- 
-def q = match(path().node(qi_node, type_node))
-	.where(not(qi_node.filter(state(StateEnum.INVALIDATED))))
- 
- def res = Neo4j.execute(q.returns(node(qi_node)))
- return res.data.collect { it.node }
